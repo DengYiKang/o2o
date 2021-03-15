92f42d484b8bce0b239d674519ac91df86c29ae6
yikang <1131487340@qq.com>
2021-03-15 19:21:02 +0800

    验证controller

--- src/main/java/com/yikang/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/yikang/o2o/web/superadmin/AreaController.java
@@ -5,8 +5,9 @@ import com.yikang.o2o.service.AreaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,11 +19,15 @@ public class AreaController {
     @Autowired
     private AreaService areaService;
 
+    @RequestMapping(value = "/listarea", method = RequestMethod.GET)
+    @ResponseBody
     private Map<String, Object> listArea() {
         Map<String, Object> modelMap = new HashMap<>();
         List<Area> list = null;
         try {
             list = areaService.getAreaList();
+            modelMap.put("rows", list);
+            modelMap.put("total", list.size());
         } catch (Exception e) {
             e.printStackTrace();
             modelMap.put("success", false);
