8b5ed3ff591ae257d1d7f2f2960c3cb1c4b174d7
yikang <1131487340@qq.com>
2021-03-16 10:28:45 +0800

    add PathUtil, ImageUtil

---
+++ src/main/java/com/yikang/o2o/util/PathUtil.java
@@ -0,0 +1,33 @@
+package com.yikang.o2o.util;
+
+public class PathUtil {
+    //获取系统的文件分隔符
+    private static String seperator = System.getProperty("file.separator");
+
+    public static String getImgBasePath() {
+        String os = System.getProperty("os.name");
+        String basePath = "";
+        if (os.toLowerCase().startsWith("win")) {
+            basePath = "D:/projectdev/image";
+        } else {
+            basePath = "/home/yikang/publicPro/";
+        }
+        basePath = basePath.replace("/", seperator);
+        return basePath;
+    }
+
+    public static String getShopImagePath(long shopId) {
+        String imagePath = "/upload/images/item/shop/" + shopId + "/";
+        return imagePath.replace("/", seperator);
+    }
+
+    public static String getHeadLineImagePath() {
+        String imagePath = "/upload/images/item/headtitle/";
+        return imagePath.replace("/", seperator);
+    }
+
+    public static String getShopCategoryPath() {
+        String imagePath = "/upload/images/item/shopcategory/";
+        return imagePath.replace("/", seperator);
+    }
+}
