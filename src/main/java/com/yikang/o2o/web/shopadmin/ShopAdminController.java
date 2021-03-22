e2009b8eb1ca49a2188fb525890d1de320195df4
yikang <1131487340@qq.com>
2021-03-22 21:15:25 +0800

    商品添加编辑后端实现

--- src/main/java/com/yikang/o2o/web/shopadmin/ShopAdminController.java
+++ src/main/java/com/yikang/o2o/web/shopadmin/ShopAdminController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * 主要用来解析路由并转发到相应的html中
+ */
 @Controller
 @RequestMapping(value = "/shopadmin", method = {RequestMethod.GET})
 public class ShopAdminController {
@@ -26,5 +29,10 @@ public class ShopAdminController {
     @RequestMapping(value = "/productcategorymanagement", method = RequestMethod.GET)
     public String productCategoryManagement() {
         return "shop/productcategorymanagement";
+    }
+
+    @RequestMapping(value = "/productoperation")
+    public String productOperation() {
+        return "shop/productoperation";
     }
 }
